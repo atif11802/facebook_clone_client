149199a2adbf88c505be75f7cb58836cd1d21fa6
ratul <ratul0947@gmail.com>
2023-02-15 18:32:17 +0600

    package.json updated

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,10 @@ import axios from "axios";
 const instance = axios.create({
 	// baseURL: "http://localhost:8000/api/",
 	baseURL: "https://fakebookserver.onrender.com/api/",
+
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
 
 export default instance;
